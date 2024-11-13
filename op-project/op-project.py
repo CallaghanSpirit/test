4f3c3d58ed9f9fac217c0e2ac97ce3a782d900b8
Alan Nouman <tsoloevalan@gmail.com>
2024-11-13 18:00:45 +0300

    add context menu

--- op-project/op-project.py
+++ op-project/op-project.py
@@ -1,16 +1,27 @@
 import wx
 APP_EXIT = 1
+VIEW_STATUS = 2
+VIEW_RGB = 3
+VIEW_SRGB = 4
+IT_MAX = 5
 
-class MyFrame(wx.MDIParentFrame):
+class MyFrame(wx.Frame):
     def  __init__(self, parent, title):
         super().__init__(parent, title = title)
 
         menubar = wx.MenuBar()
         filemenu = wx.Menu()
 
+        expmenu = wx.Menu()
+        expmenu.Append(wx.ID_ANY,"Экспорт изображения")
+        expmenu.Append(wx.ID_ANY,"Экспорт видео")
+        expmenu.Append(wx.ID_ANY,"Экспорт даннных")
+
         filemenu.Append(wx.ID_EXIT, 'Новый\tCtrl+N', )
         filemenu.Append(wx.ID_EXIT, 'Открыть\tCtrl+O', )
         filemenu.Append(wx.ID_EXIT, 'Сохранить\tCtrl+S',)
+        filemenu.AppendSubMenu(expmenu, "&Экспорт")
+        filemenu.AppendSeparator()
         
         item = wx.MenuItem(filemenu, APP_EXIT, 'Выход\tCtrl+Q', 'Выход из приложения')
         item.SetBitmap(wx.Bitmap('op-project/exit.png'))
@@ -22,10 +33,29 @@ class MyFrame(wx.MDIParentFrame):
 
         self.SetMenuBar(menubar)
 
+        self.Bind(wx.EVT_RIGHT_DOWN, self.on_right_click)
         self.Bind(wx.EVT_MENU, self.onQuit, id=APP_EXIT)
+
+    
+        self.ctx = wx.Menu()
+        it_min = self.ctx.Append(wx.ID_ANY, 'Минизировать')
+        it_max = self.ctx.Append(wx.ID_ANY, 'Распахнуть')
+
+
+    def onStatus(self, event):
+         if self.vStatus.IsChecked():
+            print('Показать статусную строку')
+         else:
+            print("Скрыть статусную строку ")
+    
+    def on_right_click(self, event):
+        # Показываем контекстное меню
+        self.PopupMenu(self.ctx, event.GetPosition())
+
 
     def onQuit(self, event):
         self.Close()
+
 
 
 app = wx.App()
