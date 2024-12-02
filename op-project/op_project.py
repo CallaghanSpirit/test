3348fab2e3c97dcd1968616867d5aef2d1f6c28b
Alan Nouman <tsoloevalan@gmail.com>
2024-12-02 18:26:11 +0300

    add test frame

--- op-project/op_project.py
+++ op-project/op_project.py
@@ -21,10 +21,9 @@ class AppContextMenu(wx.Menu):
         self.parent.Iconize()
 
     def onMaximaze(self,event):
-        self.parent.Maximize()
+        self.parent.Maximize() 
 
-
-class MyFrame(wx.Frame):
+class MainFrame(wx.Frame):
     def  __init__(self, parent, style, title):
         super().__init__(parent, title = title, style = style, size=(731,385))
         
@@ -95,10 +94,50 @@ class MyFrame(wx.Frame):
         self.Close()
 
 
+class TestFrame(wx.Frame):
+    def __init__(self, *args, **kw):
+        super().__init__(*args, **kw, size=(731,385))
+
+        panel = wx.Panel(self)
+        vbox = wx.BoxSizer(wx.VERTICAL)
+
+        font = wx.SystemSettings.GetFont(wx.SYS_DEFAULT_GUI_FONT)
+        font.SetPointSize(12)
+        panel.SetFont(font)
+
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        st1 = wx.StaticText(panel, label='Путь к файлу:')
+        tc = wx.TextCtrl(panel)
+
+        hbox.Add(st1, flag=wx.RIGHT , border=8)
+        hbox.Add(tc, proportion=1)
+
+        vbox.Add(hbox, flag=wx.EXPAND | wx.LEFT | wx.RIGHT | wx.TOP , border=10)
+        
+        st2 = wx.StaticText(panel, label='Содержимое файла')
+        vbox.Add(st2, flag=wx.EXPAND | wx.ALL, border=10)
+
+        tc2 = wx.TextCtrl(panel, style=wx.TE_MULTILINE)
+        vbox.Add(tc2, proportion=1, flag=wx.LEFT | wx.RIGHT | wx.BOTTOM | wx.EXPAND, border=10)
+        
+        btnOK = wx.Button(panel, label='Да', size=(70,30))
+        btnCN = wx.Button(panel, label='Отмена', size=(70,30))
+        
+        hbox2 = wx.BoxSizer(wx.HORIZONTAL)
+        hbox2.Add(btnOK, flag=wx.LEFT, border=10)
+        hbox2.Add(btnCN, flag=wx.LEFT, border=10)
+
+        vbox.Add(hbox2, flag=wx.ALIGN_RIGHT | wx.BOTTOM | wx.RIGHT, border=10)
+
+        panel.SetSizer(vbox)
 
 app = wx.App()
 
-frame = MyFrame(None, title = 'Мотивация', style = wx.DEFAULT_FRAME_STYLE )
+
+frame2=TestFrame(None, title = 'Test', style = wx.DEFAULT_FRAME_STYLE)
+frame2.Show()
+
+frame = MainFrame(None, title = 'Мотивация', style = wx.DEFAULT_FRAME_STYLE )
 frame.Show()
 frame.Move(-1,-1)
 
